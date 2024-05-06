5090693f6d8fbbe8319595752f710d952b44ce5c
Luciana Diaz <124196373+Lucessy@users.noreply.github.com>
2024-05-06 23:28:31 +0200

    Limpieza de la clase Servidor

--- Aeropuertos/src/aeropuertos/Servidor.java
+++ Aeropuertos/src/aeropuertos/Servidor.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
